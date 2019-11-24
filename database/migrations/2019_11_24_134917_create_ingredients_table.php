5a0d3dbdc498ca10fe5c3d1f3038f2b8625503f5
ChristineZedday <christine_bache@yahoo.fr>
2019-11-24 14:57:08 +0100

    table ingrédients

---
+++ database/migrations/2019_11_24_134917_create_ingredients_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateIngredientsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('ingredients', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->timestamps();
+            $table->string('nom_ingredient');
+            $table->foreign('saison_id')->references('id')->on('saisons')->nullable;
+            $table->foreign('insecte_id')->references('id')->on('insectes')->nullable;
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('ingredients');
+    }
+}
