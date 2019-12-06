8a175b24a1dcd0edc89e8e0f0f1a5a527dfb5886
Christine ZEDDAY <czedday@FORMATION.lan>
2019-12-06 16:32:16 +0100

    usercontroller

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,15 +3,19 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\Http\Middleware\Admin;
+use App\User;
 
 class UserController extends Controller
 {
    public function verify(Request $request)
    {
-    if (Auth::attempt(['email' => $email, 'password' => $password]))
+    if (Auth::attempt(['email' => $request->email, 'password' => $request->password]))
         {
             return view('membres.dashboard');
         }
+        else { return view('pages.registration');}
     }
 
 
@@ -20,11 +24,13 @@ class UserController extends Controller
     $validated = $request->validate([
         'name' => 'string|required',
         'email' => 'string|required',
-        'password' => 'password:api|required',
+        'password' => 'string|required',
     ]);
 
     $user = new User;
     $user->fill($validated);
+    $user->role = null;
+    $user->password = bcrypt($user->password);
 
     if ($user->save()) {
         $user->session()->flash('status',"membre enregistré avec succès");
@@ -33,14 +39,4 @@ class UserController extends Controller
     }
 }
 
-
-public function __construct()
-{
-    $this->middleware('auth');
 }
-
-/**
- * Show the application dashboard.
- *
- * @return \Illuminate\Contracts\Support\Renderable
- */
