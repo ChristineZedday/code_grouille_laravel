1835191c115148868b935cfdcb09cbddf278cf3a
Muriel GUILLOU <mguillou@FORMATION.lan>
2019-12-11 17:18:46 +0100

    ok

--- database/seeds/ActusTableSeeder.php
+++ database/seeds/ActusTableSeeder.php
@@ -12,18 +12,28 @@ class ActusTableSeeder extends Seeder
     public function run()
     {
         DB::table('actus')->insert([
+            'titre_actu' => "Dégustation de chocolats pour Noël",
+            'resume_actu' => "L'association organise une dégustation de chocolats aux insectes le 20 décembre à l'espace culturel de Montegrillon",
+            'texte_actu' => "Venez déguster les bons chocolats aux insectes préparés par l'association 'Grouille' le 20 décembre prochain entre 14h00 et 15h00",
+
+        ]);
+
+        DB::table('actus')->insert([
             'titre_actu' => "Scandale: de la viande de scarabée dans les lasagnes",
             'resume_actu' => "Un scandale à Trou-les-bouses-en-Cambrousse, on a découvert que les lasagnes produites par l'usine agro-alimentaire qui fait la fierté du pays",
             'texte_actu' => "Un scandale à Trou-les-bouses-en-Cambrousse, on a découvert que les lasagnes produites par l'usine agro-alimentaire qui fait la fierté du pays contenait une forte proportion de scarabée, importé frauduleusement des pays de l'Ouest. Cette usine étant la principale pourvoyeuse d'emplois de la régio depuis la fermeture des pinces à linge Hopdebec, la consternation est immense à Trou et ses environs.",
-            
-            
+
+
         ]);
         DB::table('actus')->insert([
             'titre_actu' => "Brigitte Bardot s'insurge contre l'abattage rituel des grillons",
             'resume_actu' => "Brigitte Bardot mène une campagne contre l'insecterie halal de Trifouillis-les-oies qui décapite les grillons sans étourdissement préalable",
             'texte_actu' => "Brigitte Bardot mène une campagne contre l'insecterie halal de Trifouillis-les-oies qui décapite les grillons sans étourdissement préalable. C'est la L214 qui a alerté la célèbre actrice sur les cruautés que subissent les malheureux grillons dans l'insecterie. Le patron se défend en arguant que l'étourdissement électrique carbonise les insectes et les rend totalement impropres à la consommation.",
-            
-            
+
+
         ]);
+
+
+
     }
 }
--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -16,10 +16,10 @@
 <div class='home'>
     <article>
         <img class='lineleft' src="../img/picto/line.png"/>
-        <h2 class='title'>Actu home 1</h2>
+        <h2 class='title'>Dernière actu</h2>
         <img class='imgarticle' src="../img/img1.jpg"/>
         <div class='article'>
-            <h3 class='subarticle'>Titre Article</h3>
+            <h3 class='subarticle'>{{ $actus->titre }}</h3>
             <p>Vestibulum ac diam sit amet quam vehicula elementum sed sit amet dui. Curabitur arcu erat, accumsan id imperdiet et, porttitor at sem. Nulla porttitor accumsan tincidunt. Donec rutrum congue leo eget malesuada. Curabitur aliquet quam id dui posuere blandit. Vivamus suscipit tortor eget felis porttitor volutpat. </p>
             <a hre=''>Read more</a>
         </div>
