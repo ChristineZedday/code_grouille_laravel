bd81fd493be93f0f7746ce1b297d8d98ec66aaf4
Muriel GUILLOU <mguillou@FORMATION.lan>
2019-12-11 14:24:38 +0100

    bbvcbv

--- app/Http/Controllers/frontInfoController.php
+++ app/Http/Controllers/frontInfoController.php
@@ -3,8 +3,14 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Info;
 
 class frontInfoController extends Controller
 {
-    //
+    public function showmention($id)
+    {
+        // $mention = Info[1];
+
+        return view('pages.mentions');
+    }
 }
