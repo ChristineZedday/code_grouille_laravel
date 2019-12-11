39e4aadb88591a5c3d4de699fcbc41fc2a498f27
Muriel GUILLOU <mguillou@FORMATION.lan>
2019-12-11 12:01:54 +0100

    back modifié

--- resources/views/includes/menuback.blade.php
+++ resources/views/includes/menuback.blade.php
@@ -1,17 +1,20 @@
 
 <nav id='menu'>
     <ul>
-        <li><a href='/back/insectes'>Insectes</a></li>
+
         <li><a href='/back/recettes'>Recettes</a></li>
-        <li><a href='/back/actus'>Actus</a></li>
-        <li><a href='/back/partenaires'>Partenaires</a></li>
-        <li><a href='/back/ingredients'>Ingrédients</a></li>
+        <li><a href='/back/commentaires'>Commentaires Recettes</a></li>
+
 
     <span>
-        <li><a href='/back/commentaires'>Commentaires</a></li>
-        <li><a href='/back/membres'>Membres</a></li>
-        <li><a href='/back/apropos'>A propos</a></li>
-        <li><a href='/back/mentions'>Mentions légales</a></li>
+        <li><a href='/back/actus'>Actus</a></li>
+        <li><a href='/back/insectes'>Liste des Insectes</a></li>
+        <li><a href='/back/ingredients'>Liste des Ingrédients</a></li>
+        <li><a href='/back/membres'>Gestion des Membres</a></li>
+        <li><a href='/back/infos'>A propos</a></li>
+        <li><a href='/back/infos'>Mentions légales</a></li>
+        <li><a href='/back/partenaires'>Partenaires</a></li>
+
     </span>
 
     </ul>
