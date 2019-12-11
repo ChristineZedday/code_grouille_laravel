35e3353f9473e15db9f84186bdad3cf65e90ea76
Muriel GUILLOU <mguillou@FORMATION.lan>
2019-12-11 16:32:55 +0100

    page à propos

--- database/seeds/InfosTableSeeder.php
+++ database/seeds/InfosTableSeeder.php
@@ -17,8 +17,10 @@ class InfosTableSeeder extends Seeder
             ]);
 
         DB::table('infos')->insert([
-            'titre' => "Article 1 à propos",
-            'texte' => "Texte article 1",
+            'titre' => "L'association Grouille",
+            'texte' => "Dans notre association, nous aimons les insectes y compris dans notre assiette. L'ojectif de notre association est de promouvoir les insectes souvent mal connus ou mal aimés.
+            Ces derniers sont généralement considérés comme des animaux nuisibles qu'on cherche à éliminer par tous les moyens et notamment en utilisant des pesticides.
+            Mais beaucoup d'insectes sont comestibles et sont très riches en protéines. Ce serait dommage de ne pas en profiter.",
             ]);
 
         DB::table('infos')->insert([
