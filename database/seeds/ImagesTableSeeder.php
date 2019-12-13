d0834b4e1115bdd5d98d359d18e0dfbf61674e34
ChristineZedday <christine_bache@yahoo.fr>
2019-12-13 22:35:11 +0100

    images seeder avoir toutes m^ img

--- database/seeds/ImagesTableSeeder.php
+++ database/seeds/ImagesTableSeeder.php
@@ -1,32 +1,458 @@
 <?php
 
 use Illuminate\Database\Seeder;
-use App\Image;
 
 class ImagesTableSeeder extends Seeder
 {
+
     /**
-     * Run the database seeds.
+     * Auto generated seed file
      *
      * @return void
      */
     public function run()
     {
-        $chemin_dossier=public_path('') .'/img/';
-        $images = scandir($chemin_dossier);
+        
 
-
-
-        foreach ($images as $image)
-        {
-            $extension = Image::fichier_type($image);
-            if ($extension == "jpg" || $extension == "png" || $extension == "gif")
-
-     {  DB::table('images')->insert([
-            'chemin_image' => $image
-
-        ]);}
-
-        }
+        \DB::table('images')->delete();
+        
+        \DB::table('images')->insert(array (
+            0 => 
+            array (
+                'id' => 1,
+                'created_at' => NULL,
+                'updated_at' => NULL,
+                'chemin_image' => '1.png',
+            ),
+            1 => 
+            array (
+                'id' => 2,
+                'created_at' => NULL,
+                'updated_at' => NULL,
+                'chemin_image' => '2.png',
+            ),
+            2 => 
+            array (
+                'id' => 3,
+                'created_at' => NULL,
+                'updated_at' => NULL,
+                'chemin_image' => '27e092f5e87e2b066b0f8ef93e56ec75.jpg',
+            ),
+            3 => 
+            array (
+                'id' => 4,
+                'created_at' => NULL,
+                'updated_at' => NULL,
+                'chemin_image' => '290px-Dytiscus_marginalis_male-female.png',
+            ),
+            4 => 
+            array (
+                'id' => 5,
+                'created_at' => NULL,
+                'updated_at' => NULL,
+                'chemin_image' => '29fdd34431d9780a468a5b375783d579.jpg',
+            ),
+            5 => 
+            array (
+                'id' => 6,
+                'created_at' => NULL,
+                'updated_at' => NULL,
+                'chemin_image' => '3.png',
+            ),
+            6 => 
+            array (
+                'id' => 7,
+                'created_at' => NULL,
+                'updated_at' => NULL,
+                'chemin_image' => '328616068a28a25b04292c52d5b19cab.jpg',
+            ),
+            7 => 
+            array (
+                'id' => 8,
+                'created_at' => NULL,
+                'updated_at' => NULL,
+                'chemin_image' => '398c74d2b36f26ac569533da1c615366.jpg',
+            ),
+            8 => 
+            array (
+                'id' => 9,
+                'created_at' => NULL,
+                'updated_at' => NULL,
+                'chemin_image' => '3b6970400e02b86297214ea7e35b69f2.jpg',
+            ),
+            9 => 
+            array (
+                'id' => 10,
+                'created_at' => NULL,
+                'updated_at' => NULL,
+                'chemin_image' => '4.png',
+            ),
+            10 => 
+            array (
+                'id' => 11,
+                'created_at' => NULL,
+                'updated_at' => NULL,
+                'chemin_image' => '44f27663de9239e887e4fb1a5439e3c7.jpg',
+            ),
+            11 => 
+            array (
+                'id' => 12,
+                'created_at' => NULL,
+                'updated_at' => NULL,
+                'chemin_image' => '5.png',
+            ),
+            12 => 
+            array (
+                'id' => 13,
+                'created_at' => NULL,
+                'updated_at' => NULL,
+                'chemin_image' => '5f0b3a139e58597dedbf2cf94935121f.jpg',
+            ),
+            13 => 
+            array (
+                'id' => 14,
+                'created_at' => NULL,
+                'updated_at' => NULL,
+                'chemin_image' => '6.png',
+            ),
+            14 => 
+            array (
+                'id' => 15,
+                'created_at' => NULL,
+                'updated_at' => NULL,
+                'chemin_image' => '71f544a244c701f479f1bd16c7d19d8f.jpg',
+            ),
+            15 => 
+            array (
+                'id' => 16,
+                'created_at' => NULL,
+                'updated_at' => NULL,
+                'chemin_image' => '7866115b24dc478b9c53883ff0b86010.jpg',
+            ),
+            16 => 
+            array (
+                'id' => 17,
+                'created_at' => NULL,
+                'updated_at' => NULL,
+                'chemin_image' => '88969babbd1be1fe2e6c712de2a64fc5.jpg',
+            ),
+            17 => 
+            array (
+                'id' => 18,
+                'created_at' => NULL,
+                'updated_at' => NULL,
+                'chemin_image' => 'Canyon-Apache-fourmis-miel.jpg',
+            ),
+            18 => 
+            array (
+                'id' => 19,
+                'created_at' => NULL,
+                'updated_at' => NULL,
+                'chemin_image' => 'Canyon-Apache-miel2.jpg',
+            ),
+            19 => 
+            array (
+                'id' => 20,
+                'created_at' => NULL,
+                'updated_at' => NULL,
+                'chemin_image' => 'YouTube-logo-light.png',
+            ),
+            20 => 
+            array (
+                'id' => 21,
+                'created_at' => NULL,
+                'updated_at' => NULL,
+                'chemin_image' => 'about.jpg',
+            ),
+            21 => 
+            array (
+                'id' => 22,
+                'created_at' => NULL,
+                'updated_at' => NULL,
+                'chemin_image' => 'about2.jpg',
+            ),
+            22 => 
+            array (
+                'id' => 23,
+                'created_at' => NULL,
+                'updated_at' => NULL,
+                'chemin_image' => 'about3.jpg',
+            ),
+            23 => 
+            array (
+                'id' => 24,
+                'created_at' => NULL,
+                'updated_at' => NULL,
+                'chemin_image' => 'actu1 3.jpg',
+            ),
+            24 => 
+            array (
+                'id' => 25,
+                'created_at' => NULL,
+                'updated_at' => NULL,
+                'chemin_image' => 'actu3_1.jpg',
+            ),
+            25 => 
+            array (
+                'id' => 26,
+                'created_at' => NULL,
+                'updated_at' => NULL,
+                'chemin_image' => 'annexe-paq-insectes-scarabee.jpg',
+            ),
+            26 => 
+            array (
+                'id' => 27,
+                'created_at' => NULL,
+                'updated_at' => NULL,
+                'chemin_image' => 'brochettes.jpg',
+            ),
+            27 => 
+            array (
+                'id' => 28,
+                'created_at' => NULL,
+                'updated_at' => NULL,
+                'chemin_image' => 'burger.jpg',
+            ),
+            28 => 
+            array (
+                'id' => 29,
+                'created_at' => NULL,
+                'updated_at' => NULL,
+                'chemin_image' => 'c8d5504358254c41237bca3e2e9f26c0.jpg',
+            ),
+            29 => 
+            array (
+                'id' => 30,
+                'created_at' => NULL,
+                'updated_at' => NULL,
+                'chemin_image' => 'd0e5791126f8bf9de4a7c90d6ef9c19d.jpg',
+            ),
+            30 => 
+            array (
+                'id' => 31,
+                'created_at' => NULL,
+                'updated_at' => NULL,
+                'chemin_image' => 'depositphotos_90294246-stock-illustration-ants-vintage-engraving.jpg',
+            ),
+            31 => 
+            array (
+                'id' => 32,
+                'created_at' => NULL,
+                'updated_at' => NULL,
+                'chemin_image' => 'eac1b1bcfc9e6eca797e01aba058a07e.jpg',
+            ),
+            32 => 
+            array (
+                'id' => 33,
+                'created_at' => NULL,
+                'updated_at' => NULL,
+                'chemin_image' => 'f3c56a84ec61b2e65ad85fb85a14f276.jpg',
+            ),
+            33 => 
+            array (
+                'id' => 34,
+                'created_at' => NULL,
+                'updated_at' => NULL,
+                'chemin_image' => 'f4a09da7c84c233f2056432d9005eae4.jpg',
+            ),
+            34 => 
+            array (
+                'id' => 35,
+                'created_at' => NULL,
+                'updated_at' => NULL,
+                'chemin_image' => 'hand-drawn-vector-pattern-with-insects-olga-olmix.jpg',
+            ),
+            35 => 
+            array (
+                'id' => 36,
+                'created_at' => NULL,
+                'updated_at' => NULL,
+                'chemin_image' => 'il_794xN.1149214818_owgs.jpg',
+            ),
+            36 => 
+            array (
+                'id' => 37,
+                'created_at' => NULL,
+                'updated_at' => NULL,
+                'chemin_image' => 'il_794xN.1195819529_9w8v.jpg',
+            ),
+            37 => 
+            array (
+                'id' => 38,
+                'created_at' => NULL,
+                'updated_at' => NULL,
+                'chemin_image' => 'il_794xN.1195819547_4kej.jpg',
+            ),
+            38 => 
+            array (
+                'id' => 39,
+                'created_at' => NULL,
+                'updated_at' => NULL,
+                'chemin_image' => 'images.png',
+            ),
+            39 => 
+            array (
+                'id' => 40,
+                'created_at' => NULL,
+                'updated_at' => NULL,
+                'chemin_image' => 'img1.jpg',
+            ),
+            40 => 
+            array (
+                'id' => 41,
+                'created_at' => NULL,
+                'updated_at' => NULL,
+                'chemin_image' => 'img2.jpg',
+            ),
+            41 => 
+            array (
+                'id' => 42,
+                'created_at' => NULL,
+                'updated_at' => NULL,
+                'chemin_image' => 'img3.jpg',
+            ),
+            42 => 
+            array (
+                'id' => 43,
+                'created_at' => NULL,
+                'updated_at' => NULL,
+                'chemin_image' => 'imgheader.jpg',
+            ),
+            43 => 
+            array (
+                'id' => 44,
+                'created_at' => NULL,
+                'updated_at' => NULL,
+                'chemin_image' => 'insecte2.jpg',
+            ),
+            44 => 
+            array (
+                'id' => 45,
+                'created_at' => NULL,
+                'updated_at' => NULL,
+                'chemin_image' => 'intro.jpg',
+            ),
+            45 => 
+            array (
+                'id' => 46,
+                'created_at' => NULL,
+                'updated_at' => NULL,
+                'chemin_image' => 'istockphoto-629061844-1024x1024.jpg',
+            ),
+            46 => 
+            array (
+                'id' => 47,
+                'created_at' => NULL,
+                'updated_at' => NULL,
+                'chemin_image' => 'istockphoto-640098816-1024x1024.jpg',
+            ),
+            47 => 
+            array (
+                'id' => 48,
+                'created_at' => NULL,
+                'updated_at' => NULL,
+                'chemin_image' => 'istockphoto-904803080-1024x1024.jpg',
+            ),
+            48 => 
+            array (
+                'id' => 49,
+                'created_at' => NULL,
+                'updated_at' => NULL,
+                'chemin_image' => 'le-discret-grillon-des-bois-se-1761x1200.jpg',
+            ),
+            49 => 
+            array (
+                'id' => 50,
+                'created_at' => NULL,
+                'updated_at' => NULL,
+                'chemin_image' => 'logo-facebook-noir-et-blanc-rond-11549682143jgnj9eha8w.png',
+            ),
+            50 => 
+            array (
+                'id' => 51,
+                'created_at' => NULL,
+                'updated_at' => NULL,
+                'chemin_image' => 'manif.jpg',
+            ),
+            51 => 
+            array (
+                'id' => 52,
+                'created_at' => NULL,
+                'updated_at' => NULL,
+                'chemin_image' => 'manif2.jpg',
+            ),
+            52 => 
+            array (
+                'id' => 53,
+                'created_at' => NULL,
+                'updated_at' => NULL,
+                'chemin_image' => 'plat.jpg',
+            ),
+            53 => 
+            array (
+                'id' => 54,
+                'created_at' => NULL,
+                'updated_at' => NULL,
+                'chemin_image' => 'reine-fourmis.jpg',
+            ),
+            54 => 
+            array (
+                'id' => 55,
+                'created_at' => NULL,
+                'updated_at' => NULL,
+                'chemin_image' => 'scorpion.jpg',
+            ),
+            55 => 
+            array (
+                'id' => 56,
+                'created_at' => NULL,
+                'updated_at' => NULL,
+                'chemin_image' => 'sugg1.jpg',
+            ),
+            56 => 
+            array (
+                'id' => 57,
+                'created_at' => NULL,
+                'updated_at' => NULL,
+                'chemin_image' => 'sugg2.jpg',
+            ),
+            57 => 
+            array (
+                'id' => 58,
+                'created_at' => NULL,
+                'updated_at' => NULL,
+                'chemin_image' => 'sugg3.jpg',
+            ),
+            58 => 
+            array (
+                'id' => 59,
+                'created_at' => NULL,
+                'updated_at' => NULL,
+                'chemin_image' => 'sugg4.jpg',
+            ),
+            59 => 
+            array (
+                'id' => 60,
+                'created_at' => NULL,
+                'updated_at' => NULL,
+                'chemin_image' => 'sushi.jpg',
+            ),
+            60 => 
+            array (
+                'id' => 61,
+                'created_at' => NULL,
+                'updated_at' => NULL,
+                'chemin_image' => 'vector-seamless-grunge-pattern-black-clip-art__k45489997.jpg',
+            ),
+            61 => 
+            array (
+                'id' => 62,
+                'created_at' => NULL,
+                'updated_at' => NULL,
+                'chemin_image' => 'youtube.png',
+            ),
+        ));
+        
+        
     }
 }
