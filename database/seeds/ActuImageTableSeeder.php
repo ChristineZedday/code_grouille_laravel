c63f9254a04b3ff5b37f794c6c07df8dce901800
Muriel GUILLOU <mguillou@FORMATION.lan>
2019-12-17 11:12:00 +0100

    commit

--- database/seeds/ActuImageTableSeeder.php
+++ database/seeds/ActuImageTableSeeder.php
@@ -12,25 +12,20 @@ class ActuImageTableSeeder extends Seeder
      */
     public function run()
     {
-        
+
 
         \DB::table('actu_image')->delete();
-        
+
         \DB::table('actu_image')->insert(array (
-            0 => 
+            0 =>
             array (
                 'id' => 1,
                 'created_at' => NULL,
                 'updated_at' => NULL,
-<<<<<<< HEAD
                 'actu_id' => 1,
                 'image_id' => 67,
-=======
-                'actu_id' => 5,
-                'image_id' => 72,
->>>>>>> 63bc4fe4a4ac584c16d31d451b812de6f59b0bd5
             ),
-            1 => 
+            1 =>
             array (
                 'id' => 2,
                 'created_at' => NULL,
@@ -38,7 +33,7 @@ class ActuImageTableSeeder extends Seeder
                 'actu_id' => 2,
                 'image_id' => 68,
             ),
-            2 => 
+            2 =>
             array (
                 'id' => 3,
                 'created_at' => NULL,
@@ -46,7 +41,7 @@ class ActuImageTableSeeder extends Seeder
                 'actu_id' => 3,
                 'image_id' => 69,
             ),
-            3 => 
+            3 =>
             array (
                 'id' => 4,
                 'created_at' => NULL,
@@ -54,24 +49,18 @@ class ActuImageTableSeeder extends Seeder
                 'actu_id' => 4,
                 'image_id' => 70,
             ),
-            4 => 
+            4 =>
             array (
                 'id' => 5,
                 'created_at' => NULL,
                 'updated_at' => NULL,
-<<<<<<< HEAD
-                'actu_id' => 5,
-                'image_id' => 71,
-            ),
-=======
                 'actu_id' => 4,
                 'image_id' => 71,
             ),
-          
-     
->>>>>>> 63bc4fe4a4ac584c16d31d451b812de6f59b0bd5
+
+
         ));
-        
-        
+
+
     }
 }
